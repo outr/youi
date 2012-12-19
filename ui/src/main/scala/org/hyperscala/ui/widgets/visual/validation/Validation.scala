5575ee951e4860ec66e101c60e3abb83e7bb2fc3
Matt Hicks <matt@outr.com>
2012-12-19 11:41:12 -0600

    Updated string validations to properly handle null.

--- ui/src/main/scala/org/hyperscala/ui/widgets/visual/validation/Validation.scala
+++ ui/src/main/scala/org/hyperscala/ui/widgets/visual/validation/Validation.scala
@@ -7,8 +7,8 @@ trait Validation[T] extends Function1[T, Either[Option[T], String]]
 
 object Validation {
   val StringEmpty = error[String]((s: String) => s != null && !s.isEmpty, "This is a required field")
-  def StringMinimumLength(minimum: Int) = error[String]((s: String) => s.length >= minimum, "Value must be at least %s characters".format(minimum))
-  def StringMaximumLength(maximum: Int) = error[String]((s: String) => s.length <= maximum, "Value must be no more than %s characters".format(maximum))
+  def StringMinimumLength(minimum: Int) = error[String]((s: String) => s != null && s.length >= minimum, "Value must be at least %s characters".format(minimum))
+  def StringMaximumLength(maximum: Int) = error[String]((s: String) => s == null || s.length <= maximum, "Value must be no more than %s characters".format(maximum))
 
   def error[T](validator: T => Boolean, message: String) = new Validation[T] {
     def apply(v: T) = if (validator(v)) {

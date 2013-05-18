93214c70ee6a120e66d4a02a6db25c7018653234
Matt Hicks <matt@outr.com>
2013-05-17 21:15:07 -0500

    Continued development of JavaScriptCombiner.

--- ui/src/main/scala/org/hyperscala/ui/module/JavaScriptCombiner.scala
+++ ui/src/main/scala/org/hyperscala/ui/module/JavaScriptCombiner.scala
@@ -1,10 +1,12 @@
 package org.hyperscala.ui.module
 
 import org.hyperscala.module.Module
-import org.powerscala.Version
-import org.hyperscala.web.site.Webpage
+import org.powerscala.{IO, Version}
+import org.hyperscala.web.site.{Website, Webpage}
 
 import org.hyperscala.html._
+import com.outr.webcommunicator.URL
+import java.io.{FileWriter, File}
 
 /**
  * @author Matt Hicks <matt@outr.com>
@@ -22,10 +24,38 @@ object JavaScriptCombiner extends Module {
         println(s"URL: ${Webpage().url}")
         scripts.foreach {
           case s => {
-            val url = Webpage().url.copy(path = s.src())
+            val url = if (s.src().toLowerCase.startsWith("http")) {
+              URL.parse(s.src())
+            } else {
+              Webpage().url.copy(path = s.src())
+            }
             println(s"Script: ${s.src()}, URL: $url")
+            val js = IO.copy(new java.net.URL(url.toString))
+            println(js)
           }
         }
+      }
+    }
+  }
+
+  private var scriptCache = Map.empty[String, String]
+  private var combinedCache = Map.empty[String, ]
+
+  private def createToken(scripts: Seq[tag.Script]) = scripts.map(s => s.src()).sorted.mkString(";")
+
+  private def loadJavaScript(uri: String) = synchronized {
+    scriptCache.get(uri) match {
+      case Some(js) => js
+      case None => {
+        val url = if (uri.toLowerCase.startsWith("http")) {
+          URL.parse(uri).getOrElse(throw new NullPointerException(s"Unable to download: $uri"))
+        } else {
+          Webpage().url.copy(path = uri)
+        }
+        val javaURL = new java.net.URL(url.toString())
+        val js = IO.copy(javaURL)
+        scriptCache += uri -> js
+        js
       }
     }
   }

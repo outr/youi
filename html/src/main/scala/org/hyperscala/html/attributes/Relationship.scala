8c8194324c4f50fd3fa8a35e334ce01fcd9b78bf
Matt Hicks <matt@outr.com>
2014-12-06 19:27:59 -0600

    Added Main and MenuItem tags to offer updated HTML5 compliance. Updated Relationship to be more flexible.

--- html/src/main/scala/org/hyperscala/html/attributes/Relationship.scala
+++ html/src/main/scala/org/hyperscala/html/attributes/Relationship.scala
@@ -8,19 +8,34 @@ import org.hyperscala.EnumEntryAttributeValue
  * NOTE: This file has been generated. Do not modify directly!
  * @author Matt Hicks <matt@outr.com>
  */
-sealed class Relationship(val value: String) extends EnumEntryAttributeValue
+class Relationship private(val value: String) extends EnumEntryAttributeValue
 
 object Relationship extends Enumerated[Relationship] with EnumEntryPersistence[Relationship] {
-  val NoReferrer = new Relationship("noreferrer")
-  val License = new Relationship("license")
-  val Bookmark = new Relationship("bookmark")
   val Alternate = new Relationship("alternate")
-  val Prefetch = new Relationship("prefetch")
-  val Tag = new Relationship("tag")
-  val NoFollow = new Relationship("nofollow")
+  val Archives = new Relationship("archives")
   val Author = new Relationship("author")
-  val Search = new Relationship("search")
+  val Bookmark = new Relationship("bookmark")
+  val External = new Relationship("external")
+  val First = new Relationship("first")
   val Help = new Relationship("help")
+  val Icon = new Relationship("icon")
+  val Index = new Relationship("index")
+  val Last = new Relationship("last")
+  val License = new Relationship("license")
   val Next = new Relationship("next")
+  val NoFollow = new Relationship("nofollow")
+  val NoReferrer = new Relationship("noreferrer")
+  val PingBack = new Relationship("pingback")
+  val Prefetch = new Relationship("prefetch")
   val Previous = new Relationship("prev")
+  val Search = new Relationship("search")
+  val StyleSheet = new Relationship("stylesheet")
+  val SideBar = new Relationship("sidebar")
+  val Tag = new Relationship("tag")
+  val Up = new Relationship("up")
+
+  override def apply(name: String) = get(name) match {
+    case Some(r) => r
+    case None => new Relationship(name)
+  }
 }
---
+++ html/src/main/scala/org/hyperscala/html/tag/MenuItem.scala
@@ -0,0 +1,39 @@
+package org.hyperscala.html.tag
+
+import org.hyperscala.Container
+import org.hyperscala.css.StyleSheet
+import org.hyperscala.html.HTMLTag
+import org.hyperscala.html.attributes.{DropZone, Draggable, Direction, ContentEditable}
+import org.hyperscala.html.constraints.BodyChild
+
+/**
+ * @author Matt Hicks <matt@outr.com>
+ */
+class MenuItem extends Container[BodyChild] with BodyChild with HTMLTag {
+  lazy val xmlLabel = "menuitem"
+  override def xmlExpanded = true
+
+  def this(name: String = null,
+           accessKey: java.lang.Character = null,
+           clazz: Seq[String] = null,
+           contentEditable: ContentEditable = null,
+           contextMenu: String = null,
+           dir: Direction = null,
+           draggable: Draggable = null,
+           dropZone: DropZone = null,
+           hidden: java.lang.Boolean = null,
+           id: String = null,
+           lang: String = null,
+           role: String = null,
+           spellCheck: java.lang.Boolean = null,
+           style: StyleSheet = null,
+           tabIndex: java.lang.Integer = null,
+           titleText: String = null,
+           content: BodyChild = null) = {
+    this()
+    init(name, accessKey, clazz, contentEditable, contextMenu, dir, draggable, dropZone, hidden, id, lang, role, spellCheck, style, tabIndex, titleText)
+    if (content != null) contents += content
+  }
+
+
+}

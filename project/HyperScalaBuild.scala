a3f4819c648d5696dad25a5263917b1ef30c8a92
Matt Hicks <matt@outr.com>
2015-02-17 08:33:18 -0600

    Fixed a bug duplicate sending property changes in tags before the tag actually renders. Updated BrowserError to not output errorMessage and stackTrace if null.

--- project/HyperScalaBuild.scala
+++ project/HyperScalaBuild.scala
@@ -67,7 +67,7 @@ object HyperScalaBuild extends Build {
 
   lazy val root = Project("root", file("."), settings = unidocSettings ++ createSettings("hyperscala-root"))
     .settings(publishArtifact in Compile := false, publishArtifact in Test := false)
-    .aggregate(core, html, javascript, svg, web, jquery, realtime, ui, contentEditor, ux, bootstrap, createJS, generator, hello, examples, numberGuess, site)
+    .aggregate(core, html, javascript, svg, web, snapSVG, jquery, jqueryUI, realtime, ui, contentEditor, ux, bootstrap, createJS, generator, hello, examples, numberGuess, site)
   lazy val core = Project("core", file("core"), settings = createSettings("hyperscala-core") ++ buildInfoSettings)
     .settings(libraryDependencies ++= Seq(outrNetCore))
     .settings(sourceGenerators in Compile <+= buildInfo,

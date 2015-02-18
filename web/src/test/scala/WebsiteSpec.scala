20b02536a72a969de53de784c1523ac54d675cf7
hrj <harshad.rj@gmail.com>
2015-02-18 15:25:15 +0530

    website session scope tests

--- web/src/test/scala/WebsiteSpec.scala
+++ web/src/test/scala/WebsiteSpec.scala
@@ -102,7 +102,66 @@ class WebsiteSpec extends WordSpec with Matchers {
         response.content.asString shouldBe ("404 Page not found: http://localhost" + url)
         website.pages.size shouldBe (1)
       }
-      
+
+    }
+
+
+    "handle session scoped requests" in {
+      val website = new Website[MapSession] {
+        protected def createSession(request: HttpRequest, id: String) = new MapSession(id, this)
+      }
+
+      val url = "/testSessionScope"
+
+      val webpage = new Webpage(website)
+      webpage.rendered shouldBe(false)
+
+
+      var sessionCount = 0
+      webpage.session {
+        sessionCount += 1
+      }
+
+      val handler = website.page(webpage, Scope.Session, url)
+
+      handler.link shouldBe (url)
+
+      {
+        val request = new HttpRequest(new URL(path = url), Method.Get, HttpRequestHeaders.Empty, Map.empty)
+        val response = website.onReceive(request, HttpResponse.NotFound)
+
+        response.status shouldEqual (HttpResponseStatus.OK)
+        response.content.asString.replaceAll("""\s""", "") shouldEqual ("""<!DOCTYPEhtml><html><head><title>Webpage</title><metaname="generator"content="Hyperscala"/><metacharset="UTF-8"/></head></html>""")
+        website.pages.size shouldBe (1)
+        website.pages.byId(webpage.pageId) should not be empty
+      }
+
+      sessionCount shouldBe (1)
+
+      {
+        // A request with pageid should trigger a cached response
+        val cachedRequest = new HttpRequest(new URL(path = url, parameters = HttpParameters(Map("pageId" -> List(webpage.pageId)))), Method.Get, HttpRequestHeaders.Empty, Map.empty)
+        val response2 = website.onReceive(cachedRequest, HttpResponse.NotFound)
+
+        response2.status shouldEqual (HttpResponseStatus.OK)
+        response2.content.asString.replaceAll("""\s""", "") shouldEqual ("""<!DOCTYPEhtml><html><head><title>Webpage</title><metaname="generator"content="Hyperscala"/><metacharset="UTF-8"/></head></html>""")
+        website.pages.size shouldBe (1)
+      }
+
+      website.removeContent(url)
+
+      {
+        val request = new HttpRequest(new URL(path = url), Method.Get, HttpRequestHeaders.Empty, Map.empty)
+        val response = website.onReceive(request, HttpResponse.NotFound)
+
+        response.status shouldEqual (HttpResponseStatus.NotFound)
+        response.content.asString shouldBe ("404 Page not found: http://localhost" + url)
+        website.pages.size shouldBe (1)
+      }
+
+      webpage.rendered shouldBe(true)
+
+      sessionCount shouldBe (2)
     }
 
   }

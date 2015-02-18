20c6ccf0a74cd2de8f04f282fbcffa97ef9e6eb6
hrj <harshad.rj@gmail.com>
2015-02-18 19:06:34 +0530

    test conversion of page to scala

--- web/src/test/scala/HTMLToScalaSpec.scala
+++ web/src/test/scala/HTMLToScalaSpec.scala
@@ -24,13 +24,30 @@ class HTMLToScalaSpec extends WordSpec with Matchers {
       webpage.body.innerHTML should fullyMatch regex """\s*Test\s*"""
     }
 
-    "convert simple html to scala" in {
+    "convert simple tag to scala" in {
       val tagStr = HTMLToScala.toScala(new tag.HTML())
       tagStr should fullyMatch regex """new tag.HTML\s*\{\s*\}\s*"""
+    }
 
-      val webpage = new Webpage(website)
+    "convert simple html to scala" in {
       val scalaStr = HTMLToScala.toScala(new tag.HTML(), "pkg", "cls")
       val expectedRegex = """\s*package pkg\s+(import .*\s*)*class cls extends tag.HTML\s*\{\s*\}\s*"""
+      scalaStr should fullyMatch regex expectedRegex
+    }
+
+    "convert simple page to scala" in {
+      val webpage = new Webpage(website)
+      webpage.html.body.contents += "test"
+      val scalaStr = HTMLToScala.toScala(webpage, "pkg", "cls")
+
+      val meta1Regex = """head.contents\s*\+=\s*new\s*tag\.Meta\(name\s*=\s*"generator",\s*content\s*=\s*"Hyperscala"\)\s*\{\s*\}"""
+      val meta2Regex = """head.contents\s*\+=\s*new\s*tag\.Meta\(charset\s*=\s*"UTF-8"\)\s*\{\s*\}"""
+      val headRegex = meta1Regex + """\s+""" + meta2Regex
+      val bodyRegex = """body.contents\s*\+=\s*"test""""
+      val titleRegex = """title\s*:=\s*"Webpage"\s+"""
+      val classRegex = titleRegex+headRegex+"\\s+"+bodyRegex
+      val expectedRegex = """\s*package pkg\s+(import .*\s*)*class cls extends Webpage\s*\{\s*"""+classRegex+"""\s*}\s*"""
+
       scalaStr should fullyMatch regex expectedRegex
     }
 

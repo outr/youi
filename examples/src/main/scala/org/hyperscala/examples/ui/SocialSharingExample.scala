add16fbf722f66be3e82906dbcdecd889b3311f5
Mark Mynsted <code@growingliberty.com>
2015-04-23 13:41:23 -0500

    Adding ability to better customize SocialSharing
    Moving most to a common trait and providing a default implementation
    that handles the simple case.  Other cases, such as when one would like
    to replace the stylesheet, can be implemented by extending
    SocialSharingBase and overriding what is needed.

--- examples/src/main/scala/org/hyperscala/examples/ui/SocialSharingExample.scala
+++ examples/src/main/scala/org/hyperscala/examples/ui/SocialSharingExample.scala
@@ -2,7 +2,6 @@ package org.hyperscala.examples.ui
 
 import org.hyperscala.examples.Example
 import org.hyperscala.html.StaticHTML
-import org.hyperscala.ui.module.SocialSharing
 import org.hyperscala.ui.module.SocialSharing._
 import org.powerscala.IO
 import org.hyperscala.html._
@@ -12,7 +11,8 @@ import org.hyperscala.html._
  * code@growingliberty.com
  */
 class SocialSharingExample extends Example {
-  this.require(SocialSharing)
+  this.require(SocialSharingModule)
+  import SocialSharingModule._
 
   val title = "Check out Hyperscala.com"
   val description = "Hyperscala is a statically typed bare-metal HTML, CSS, and JavaScript framework for Scala."
@@ -21,7 +21,7 @@ class SocialSharingExample extends Example {
 
   contents += new tag.Div {
     style.padding := "1em"
-    contents += s"Social sharing buttons (${SocialSharing.version.general}):"
+    contents += s"Social sharing buttons (${version.general}):"
     contents += new SocialSharingLinks {
       contents += new EmailButton(title, s"$description $shareLink")
       contents += new FacebookButton(shareLink)
--- ui/src/main/scala/org/hyperscala/ui/module/SocialSharing.scala
+++ ui/src/main/scala/org/hyperscala/ui/module/SocialSharing.scala
@@ -14,35 +14,7 @@ import com.outr.net.URL
  * Created by mmynsted on 4/6/15.
  * code@growingliberty.com
  */
-object SocialSharing extends Module with Logging {
-  /*
-   * Add rrssb  Ridiculously Responsive Social Sharing Buttons
-   *  http://kurtnoble.com/labs/rrssb
-   */
-  val name = "rrssb"
-
-  /*
-   * The version reflects the tagged release as found here
-   * https://github.com/kni-labs/rrssb/tags
-   */
-  val version = Version(1,7,5)
-
-  override def dependencies = List(jQuery)
-
-  override def init(website: Website) =  website.addClassPath("/rrssb/", "rrssb/")
-
-  override def load(webpage: Webpage) = {
-    webpage.head.contents += new tag.Link(href = "/rrssb/css/rrssb.css", rel = "stylesheet")
-    webpage.head.contents += new tag.Script(mimeType = "text/javascript", src = "/rrssb/js/rrssb.min.js")
-  }
-
-  def encodeLink(base: String, parms: (String, Option[String])*): String = {
-    val decURL = parms.foldLeft(URL.decoded(base))((b,a) => a match {
-      case (k, Some(v)) => b.param(k,v)
-      case _ => b
-    })
-    decURL.encoded.toString()
-  }
+object SocialSharing {
 
   implicit class MapToQueryString(val m: Map[String, String]) extends AnyVal {
     def asQueryString: String = m.foldLeft("") {
@@ -50,76 +22,110 @@ object SocialSharing extends Module with Logging {
     }
   }
 
-  class SocialSharingLinks extends tag.Ul(clazz = Seq("rrssb-buttons", "clearfix"))
+  trait SocialSharingBase extends Module with Logging {
+    /*
+     * Add rrssb  Ridiculously Responsive Social Sharing Buttons
+     *  http://kurtnoble.com/labs/rrssb
+     */
+    val name = "rrssb"
 
-  abstract class SocialSharingLink extends tag.Li {
-    def iconName: String
-    def link: String
-    def liClazzSuffix: String = iconName
-    def anchorClazz: Seq[String] = Seq("popup")
-    def anchorSpanContent: String = iconName
-    protected def createIcon: BodyChild = IO.copy(getClass.getClassLoader.getResource(s"rrssb/icons/${iconName}.svg"))
+    /*
+     * The version reflects the tagged release as found here
+     * https://github.com/kni-labs/rrssb/tags
+     */
+    val version = Version(1,7,5)
 
-    clazz += s"rrssb-$liClazzSuffix"
-    contents += new tag.A(href = link) {
-      clazz ++= anchorClazz
-      contents += new tag.Span(clazz = List("rrssb-icon")) {
-        contents += createIcon
-      }
-      contents += new Span(content = anchorSpanContent) {
-        clazz += "rrssb-text"
+    override def dependencies = List(jQuery)
+
+    def stylesheetRef = "/rrssb/css/rrssb.css"
+
+    override def init(website: Website) =  website.addClassPath("/rrssb/", "rrssb/")
+
+    override def load(webpage: Webpage) = {
+      webpage.head.contents += new tag.Link(href = stylesheetRef, rel = "stylesheet")
+      webpage.head.contents += new tag.Script(mimeType = "text/javascript", src = "/rrssb/js/rrssb.min.js")
+    }
+
+    def encodeLink(base: String, parms: (String, Option[String])*): String = {
+      val decURL = parms.foldLeft(URL.decoded(base))((b,a) => a match {
+        case (k, Some(v)) => b.param(k,v)
+        case _ => b
+      })
+      decURL.encoded.toString()
+    }
+
+    class SocialSharingLinks extends tag.Ul(clazz = Seq("rrssb-buttons", "clearfix"))
+
+    abstract class SocialSharingLink extends tag.Li {
+      def iconName: String
+      def link: String
+      def liClazzSuffix: String = iconName
+      def anchorClazz: Seq[String] = Seq("popup")
+      def anchorSpanContent: String = iconName
+      protected def createIcon: BodyChild = IO.copy(getClass.getClassLoader.getResource(s"rrssb/icons/${iconName}.svg"))
+
+      clazz += s"rrssb-$liClazzSuffix"
+      contents += new tag.A(href = link) {
+        clazz ++= anchorClazz
+        contents += new tag.Span(clazz = List("rrssb-icon")) {
+          contents += createIcon
+        }
+        contents += new Span(content = anchorSpanContent) {
+          clazz += "rrssb-text"
+        }
       }
     }
+
+    class EmailButton(subject: String, body: String) extends {
+      val iconName = "mail"
+      override val anchorClazz = Seq.empty[String]
+      override val anchorSpanContent = "email"
+      override val liClazzSuffix = "email"
+      val link = s"mailto:?${Map("subject" -> subject, "body" -> body).asQueryString}"
+    } with SocialSharingLink
+
+    /**
+     * Customization must come from Open Graph tags in the shared URL.
+     * @param u is for the link url.
+     */
+    class FacebookButton(u: String) extends {
+      val iconName = "facebook"
+      val link = encodeLink("https://www.facebook.com/sharer/sharer.php", ("u", Some(u)))
+    } with SocialSharingLink
+
+    class LinkedInButton(url: String, title: String, summary: Option[String] = None) extends {
+      val iconName = "linkedin"
+      val link = encodeLink("http://www.linkedin.com/shareArticle", ("mini", Some("true")),
+        ("url", Some(url)), ("title", Some(title)), ("summary", summary))
+    } with SocialSharingLink
+
+    class TwitterButton(status: String) extends {
+      val iconName = "twitter"
+      val link = encodeLink("http://twitter.com/home", ("status", Some(status)))
+    } with SocialSharingLink
+
+    /** Google+ identifies the parameter as "url", but it seems that it may include other content
+      *
+      * @param url the link to share as well as other information
+      */
+    class GoogleplusButton(url: String) extends {
+      val iconName = "google_plus"
+      override val liClazzSuffix = "googleplus"
+      override val anchorSpanContent = "google+"
+      val link = encodeLink("https://plus.google.com/share", ("url", Some(url)))
+    } with SocialSharingLink
+
+    class PinterestButton(url: String, description: Option[String] = None, media: Option[String] = None)
+      extends {
+        val iconName = "pinterest"
+        val link = encodeLink("http://pinterest.com/pin/create/button/", ("url" -> Some(url)),
+          ("description", description), ("media", media))
+      } with SocialSharingLink
+
+    class GithubButton(username: String, project: String) extends {
+      val iconName = "github"
+      val link = s"https://github.com/$username/$project"
+    } with SocialSharingLink
   }
-
-  class EmailButton(subject: String, body: String) extends {
-    val iconName = "mail"
-    override val anchorClazz = Seq.empty[String]
-    override val anchorSpanContent = "email"
-    override val liClazzSuffix = "email"
-    val link = s"mailto:?${Map("subject" -> subject, "body" -> body).asQueryString}"
-  } with SocialSharingLink
-
-  /**
-   * Customization must come from Open Graph tags in the shared URL.
-   * @param u is for the link url.
-   */
-  class FacebookButton(u: String) extends {
-    val iconName = "facebook"
-    val link = encodeLink("https://www.facebook.com/sharer/sharer.php", ("u", Some(u)))
-  } with SocialSharingLink
-
-  class LinkedInButton(url: String, title: String, summary: Option[String] = None) extends {
-    val iconName = "linkedin"
-    val link = encodeLink("http://www.linkedin.com/shareArticle", ("mini", Some("true")),
-      ("url", Some(url)), ("title", Some(title)), ("summary", summary))
-  } with SocialSharingLink
-  
-  class TwitterButton(status: String) extends {
-    val iconName = "twitter"
-    val link = encodeLink("http://twitter.com/home", ("status", Some(status)))
-  } with SocialSharingLink
-
-  /** Google+ identifies the parameter as "url", but it seems that it may include other content
-    *
-    * @param url the link to share as well as other information
-    */
-  class GoogleplusButton(url: String) extends {
-    val iconName = "google_plus"
-    override val liClazzSuffix = "googleplus"
-    override val anchorSpanContent = "google+"
-    val link = encodeLink("https://plus.google.com/share", ("url", Some(url)))
-  } with SocialSharingLink
-
-  class PinterestButton(url: String, description: Option[String] = None, media: Option[String] = None)
-    extends {
-    val iconName = "pinterest"
-      val link = encodeLink("http://pinterest.com/pin/create/button/", ("url" -> Some(url)),
-        ("description", description), ("media", media))
-  } with SocialSharingLink
-
-  class GithubButton(username: String, project: String) extends {
-    val iconName = "github"
-    val link = s"https://github.com/$username/$project"
-  } with SocialSharingLink
+  object SocialSharingModule extends SocialSharingBase
 }

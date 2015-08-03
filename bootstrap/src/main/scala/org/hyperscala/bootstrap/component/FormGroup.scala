420bc7ad5354b57ea242413c1fb47a131a2d4a11
Tim Nieradzik <tim@metastack.pl>
2015-08-03 16:33:34 +0200

    Bootstrap: Move FormGroup.apply()

--- bootstrap/src/main/scala/org/hyperscala/bootstrap/component/FormGroup.scala
+++ bootstrap/src/main/scala/org/hyperscala/bootstrap/component/FormGroup.scala
@@ -8,13 +8,3 @@ import org.hyperscala.html._
 class FormGroup extends tag.Div {
   clazz += "form-group"
 }
-
-object FormGroup {
-  def apply(labelText: String, field: FormField) = {
-    val group = new FormGroup
-    group.contents += new tag.Label(forElement = field.identity, content = labelText, clazz = List("col-sm-3", "control-label"))
-    field.clazz += "form-control"
-    group.contents += new tag.Div(clazz = List("col-sm-9"), content = field)
-    group
-  }
-}

e0cca9ae8d951384bc9497801458e773ec0017e4
Tim Nieradzik <tim@metastack.pl>
2015-08-06 13:10:37 +0200

    Build: Move code generation to external file

--- project/HyperScalaBuild.scala
+++ project/HyperScalaBuild.scala
@@ -1,5 +1,5 @@
-import sbt.Keys._
 import sbt._
+import sbt.Keys._
 import sbtassembly.AssemblyKeys._
 import sbtassembly._
 import sbtassembly.AssemblyPlugin._
@@ -66,75 +66,6 @@ object HyperScalaBuild extends Build {
       </developers>
   )
 
-  def between(subject: String, left: String, right: String): Option[String] = {
-    val ofs = subject.indexOf(left)
-    val end = subject.indexOf(right, ofs + left.length + 1)
-    if (ofs == -1 || end == -1) None
-    else Some(subject.slice(ofs + left.length, end))
-  }
-
-  def objectName(cssTag: String): String =
-    cssTag.foldLeft("") { case (acc, cur) =>
-      if (acc.length == 0) "" + cur.toUpper
-      else if (acc.last == '-') acc.dropRight(1) + cur.toUpper
-      else acc + cur
-    }
-
-  def generateBootstrap(sourceGen: File, webJars: File): Seq[File] = {
-    val prefix = "glyphicon"
-
-    val file = sourceGen / "org" / "hyperscala" / "bootstrap" / "component" / "Glyphicon.scala"
-
-    val objects = io.Source.fromFile(webJars / "lib" / "bootstrap" / "less" / "glyphicons.less")
-      .getLines()
-      .filter(_.startsWith(s".$prefix-"))
-      .map { line =>
-      val icon =
-        between(line, "-", "{")
-          .orElse(between(line, "-", ","))
-          .get.trim
-      val obj = objectName(icon)
-
-      s"""
-       case object $obj extends Glyphicon("$prefix-$icon")
-       """
-    }
-      .mkString("\n")
-
-    IO.write(file,
-      """
-      package org.hyperscala.bootstrap.component
-      import org.powerscala.enum.{Enumerated, EnumEntry}
-      import org.hyperscala.html.HTMLTag
-      sealed abstract class Glyphicon(val className: String) extends EnumEntry {
-        def apply[T <: HTMLTag](t: T) = {
-          t.clazz := t.clazz().filter(s => s.startsWith("glyphicon"))
-          t.clazz += "glyphicon"
-          t.clazz += s"glyphicon-$className"
-          t
-        }
-        def create() = apply(new org.hyperscala.html.tag.Span)
-      }
-      """ +
-        s"""
-      object Glyphicon extends Enumerated[Glyphicon] {
-        $objects
-        val values = findValues.toVector
-      }
-      """
-    )
-
-    Seq(file)
-  }
-
-  val extractGlyphicons = {
-    val source = sourceManaged in Compile
-    val zipped = source.zip(WebKeys.webJarsDirectory in Assets)
-    zipped.map { case (src, web) =>
-      generateBootstrap(src, web)
-    }
-  }.dependsOn(WebKeys.webJars in Assets)
-
   private def createSettings(_name: String) = baseSettings ++ assemblySettings ++ Seq(name := _name)
 
   lazy val root = Project("root", file("."), settings = unidocSettings ++ createSettings("hyperscala-root"))
@@ -184,7 +115,7 @@ object HyperScalaBuild extends Build {
   lazy val bootstrap = Project("bootstrap", file("bootstrap"), settings = createSettings("hyperscala-bootstrap"))
     .enablePlugins(SbtWeb)
     .dependsOn(ui)
-    .settings(sourceGenerators in Compile <+= extractGlyphicons)
+    .settings(sourceGenerators in Compile <+= Bootstrap.extractGlyphicons)
 
   lazy val createJS = Project("createjs", file("createjs"), settings = createSettings("hyperscala-createjs"))
     .dependsOn(ui)

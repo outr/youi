f77c3399120fe509d24612daf3d0aaa239719b9c
Matt Hicks <matt@outr.com>
2017-02-21 15:40:12 -0600

    Added support to SinglePageApplication to offer more modular integration of deltas via handlers.

--- app/jvm/src/main/scala/io/youi/app/SinglePageApplication.scala
+++ app/jvm/src/main/scala/io/youi/app/SinglePageApplication.scala
@@ -2,9 +2,10 @@ package io.youi.app
 
 import java.io.File
 
-import io.youi.http.{Content, HttpConnection}
+import io.youi.Priority
+import io.youi.http.{Content, HttpConnection, HttpRequest}
 import io.youi.net.ContentType
-import io.youi.server.handler.{CachingManager, SenderHandler}
+import io.youi.server.handler.{CachingManager, HttpHandler, HttpHandlerBuilder, SenderHandler}
 import io.youi.stream.{ByTag, Delta, HTMLParser, Selector}
 
 trait SinglePageApplication extends ServerApplication {
@@ -14,12 +15,27 @@ trait SinglePageApplication extends ServerApplication {
   protected def appJSMethod: String
   protected def scriptPaths: List[String] = Nil
   protected def responseMap(httpConnection: HttpConnection): Map[String, String] = Map.empty
-  protected def deltas(httpConnection: HttpConnection): List[Delta] = Nil
+
+  implicit class SinglePageHttpHandlerBuilder(builder: HttpHandlerBuilder) {
+    /**
+      * Stores deltas on this connection for use serving HTML.
+      *
+      * @param function the function that takes in an HttpRequest and returns a list of Deltas.
+      * @return HttpHandler that has already been added to the server
+      */
+    def deltas(function: HttpRequest => List[Delta]): HttpHandler = builder.handle { connection =>
+      val d = function(connection.request)
+      if (d.nonEmpty) {
+        val current = connection.store.getOrElse[List[Delta]](SinglePageApplication.DeltaKey, Nil)
+        connection.store(SinglePageApplication.DeltaKey) = current ::: d
+      }
+    }
+  }
 
   override protected def init(): Unit = {
     super.init()
 
-    handler.handle { httpConnection =>
+    handler.priority(Priority.Low).handle { httpConnection =>
       val url = httpConnection.request.url
       val fileName = url.path.decoded
       if (fileName.endsWith(".html") && excludeDotHTML) {
@@ -47,6 +63,7 @@ trait SinglePageApplication extends ServerApplication {
     val responseFields = responseMap(httpConnection).toList.map {
       case (name, value) => s"""<input type="hidden" id="$name" value="$value"/>"""
     }
+    val deltas = httpConnection.store.getOrElse[List[Delta]](SinglePageApplication.DeltaKey, Nil)
     val d = List(
       Delta.InsertLastChild(ByTag("body"),
         s"""
@@ -58,9 +75,13 @@ trait SinglePageApplication extends ServerApplication {
            |</script>
          """.stripMargin
       )
-    ) ::: deltas(httpConnection)
+    ) ::: deltas
     val selector = httpConnection.request.url.param("selector").map(Selector.parse)
     val html = stream.stream(d, selector)
     SenderHandler.handle(httpConnection, Content.string(html, ContentType.`text/html`), caching = CachingManager.NotCached)
   }
+}
+
+object SinglePageApplication {
+  val DeltaKey: String = "deltas"
 }

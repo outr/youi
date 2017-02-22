39677f2cec9775dac0a586fcfffebc817290e47e
Matt Hicks <matt@outr.com>
2017-02-22 17:27:46 -0600

    Added support to Template to load template path from an environment variable for compile-time loading.

--- dom/src/main/scala/io/youi/Template.scala
+++ dom/src/main/scala/io/youi/Template.scala
@@ -10,13 +10,15 @@ import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
 object Template {
-  def byId[E <: Element](path: String, id: String): E = macro TemplateMacros.existingById[E]
-  def byClass[E <: Element](path: String, className: String): List[E] = macro TemplateMacros.existingByClass[E]
+  def byId[E <: Element](path: String, id: String, appName: String): E = macro TemplateMacros.existingById[E]
+  def byClass[E <: Element](path: String, className: String, appName: String): List[E] = macro TemplateMacros.existingByClass[E]
 }
 
 @compileTimeOnly("Enable Macros for expansion")
 object TemplateMacros {
-  def existingById[E <: Element](context: blackbox.Context)(path: context.Expr[String], id: context.Expr[String])(implicit e: context.WeakTypeTag[E]): context.Expr[E] = {
+  def existingById[E <: Element](context: blackbox.Context)(path: context.Expr[String],
+                                                            id: context.Expr[String],
+                                                            appName: context.Expr[String])(implicit e: context.WeakTypeTag[E]): context.Expr[E] = {
     import context.universe._
 
     val pathValue = path match {
@@ -25,7 +27,17 @@ object TemplateMacros {
     val idValue = id match {
       case Expr(Literal(Constant(value: String))) => value
     }
-    val file = new File(pathValue)
+    val environmentVariable = appName match {
+      case Expr(Literal(Constant(value: String))) => s"${value.toUpperCase}_TEMPLATE_PATH"
+    }
+    val templatePath = Option(System.getenv(environmentVariable))
+    val file = templatePath match {
+      case Some(basePath) => new File(basePath, pathValue)
+      case None => {
+        context.warning(context.enclosingPosition, s"No environment variable defined for $environmentVariable.")
+        new File(pathValue)
+      }
+    }
     if (!file.exists()) {
       context.abort(context.enclosingPosition, s"Unable to find path for ${file.getAbsolutePath}.")
     }
@@ -38,7 +50,9 @@ object TemplateMacros {
        """)
   }
 
-  def existingByClass[E <: Element](context: blackbox.Context)(path: context.Expr[String], className: context.Expr[String])(implicit e: context.WeakTypeTag[E]): context.Expr[List[E]] = {
+  def existingByClass[E <: Element](context: blackbox.Context)(path: context.Expr[String],
+                                                               className: context.Expr[String],
+                                                               appName: context.Expr[String])(implicit e: context.WeakTypeTag[E]): context.Expr[List[E]] = {
     import context.universe._
 
     val pathValue = path match {
@@ -47,9 +61,16 @@ object TemplateMacros {
     val classValue = className match {
       case Expr(Literal(Constant(value: String))) => value
     }
-    val file = new File(pathValue)
+    val environmentVariable = appName match {
+      case Expr(Literal(Constant(value: String))) => s"${value.toUpperCase}_TEMPLATE_PATH"
+    }
+    val templatePath = Option(System.getenv(environmentVariable))
+    val file = templatePath match {
+      case Some(basePath) => new File(basePath, pathValue)
+      case None => new File(pathValue)
+    }
     if (!file.exists()) {
-      context.abort(context.enclosingPosition, s"Unable to find path for ${file.getAbsolutePath}.")
+      context.abort(context.enclosingPosition, s"Unable to find path for ${file.getAbsolutePath}, $environmentVariable=$templatePath.")
     }
     val parser = HTMLParser(file)
     val template = parser.stream(Nil, selector = Some(ByClass(classValue)), includeAllMatches = true)

26878e9271d6e2023bbe21cf0997af5cc49cca5a
Matt Hicks <matt@outr.com>
2017-03-17 11:44:40 -0500

    Temporary work-around for slightly buggy scrollbar calculations.

--- ui/src/main/scala/io/youi/hypertext/Container.scala
+++ ui/src/main/scala/io/youi/hypertext/Container.scala
@@ -7,8 +7,9 @@ class Container(val element: html.Element) extends HTMLContainer with Component 
   def this() = this(dom.create[html.Element]("div"))
 
   // Default size to be based on children's max dimensions
-  size.width := (0.0 :: children.map(_.position.right()).toList).max + scrollbar.vertical.size()
-  size.height := (0.0 :: children.map(_.position.bottom()).toList).max + scrollbar.horizontal.size()
+  // TODO: fix scrollbar size issue - seems to be including border size?
+  size.width := (0.0 :: children.map(_.position.right()).toList).max // + scrollbar.vertical.size()
+  size.height := (0.0 :: children.map(_.position.bottom()).toList).max // + scrollbar.horizontal.size()
 
   init()
 }

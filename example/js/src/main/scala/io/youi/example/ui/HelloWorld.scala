5a61ee156d07877799d9d0141dbe87d396bd26c3
Matt Hicks <matt@outr.com>
2017-04-19 10:57:03 -0500

    Mapped all TextStyle properties to Text component.

--- example/js/src/main/scala/io/youi/example/ui/HelloWorld.scala
+++ example/js/src/main/scala/io/youi/example/ui/HelloWorld.scala
@@ -3,7 +3,6 @@ package io.youi.example.ui
 import io.youi._
 import io.youi.app.screen.UIScreen
 import io.youi.component.Text
-import io.youi.style.{GradientDirection, GradientFill, GradientStop}
 
 object HelloWorld extends UIExampleScreen with UIScreen {
   override def name: String = "Hello World"
@@ -13,12 +12,7 @@ object HelloWorld extends UIExampleScreen with UIScreen {
     container.children += new Text {
       value := "Hello, World!"
       font.size := 48.0
-//      fill := Color.DarkBlue
-      fill := new GradientFill(GradientDirection.Horizontal, Vector(
-        GradientStop(Color.Red, 0.0),
-        GradientStop(Color.Green, 0.25),
-        GradientStop(Color.Blue, 1.0)
-      ))
+      fill := Color.DarkBlue
       position.center := renderer.position.center
       position.middle := renderer.position.middle
     }
--- ui/src/main/scala/io/youi/component/Text.scala
+++ ui/src/main/scala/io/youi/component/Text.scala
@@ -9,10 +9,33 @@ class Text extends Component {
   override protected[component] lazy val instance: PIXI.Text = new PIXI.Text("")
 
   lazy val value: Var[String] = Var.bound(instance.text, (s: String) => instance.text = s)
-  object font {
-    lazy val size: Var[Double] = Var.bound(12.0, (d: Double) => instance.style.fontSize = d, setImmediately = true)
+
+  lazy val breakWords: Var[Boolean] = Var.bound(false, (b: Boolean) => instance.style.breakWords = b, setImmediately = true)
+  object dropShadow {
+    lazy val enabled: Var[Boolean] = Var.bound(false, (b: Boolean) => instance.style.dropShadow = b, setImmediately = true)
+    lazy val angle: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.dropShadowAngle = d, setImmediately = true)
+    lazy val blur: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.dropShadowBlur = d, setImmediately = true)
+    lazy val color: Var[Color] = Var.bound(Color.Black, (c: Color) => instance.style.dropShadowColor = c.hex, setImmediately = true)
+    lazy val distance: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.dropShadowDistance = d, setImmediately = true)
   }
   lazy val fill: Var[Fill] = Var.bound(Color.Black, (f: Fill) => f.fill(instance.style), setImmediately = true)
+  object font {
+    lazy val family: Var[String] = Var.bound("", (s: String) => instance.style.fontFamily = s)
+    lazy val size: Var[Double] = Var.bound(12.0, (d: Double) => instance.style.fontSize = d, setImmediately = true)
+    lazy val style: Var[String] = Var.bound("", (s: String) => instance.style.fontStyle = s)
+    lazy val variant: Var[String] = Var.bound("", (s: String) => instance.style.fontVariant = s)
+    lazy val weight: Var[String] = Var.bound("", (s: String) => instance.style.fontWeight = s)
+  }
+  lazy val letterSpacing: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.letterSpacing = d)
+  lazy val lineHeight: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.lineHeight = d)
+  lazy val lineJoin: Var[String] = Var.bound("", (s: String) => instance.style.lineJoin = s)
+  lazy val miterLimit: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.miterLimit = d)
+  lazy val padding: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.padding = d)
+  lazy val stroke: Var[Color] = Var.bound(Color.Clear, (c: Color) => instance.style.stroke = c.hex)
+  lazy val strokeThickness: Var[Double] = Var.bound(0.0, (d: Double) => instance.style.strokeThickness = d)
+  lazy val textBaseline: Var[String] = Var.bound("", (s: String) => instance.style.textBaseline = s)
+  val wordWrap: Var[Boolean] = Var.bound(false, (b: Boolean) => instance.style.wordWrap = b, setImmediately = true)
+  val wordWrapWidth: Var[Double] = Var.bound(size.width(), (d: Double) => instance.style.wordWrapWidth = d)
 
   value.on(measure())
   font.size.on(measure())

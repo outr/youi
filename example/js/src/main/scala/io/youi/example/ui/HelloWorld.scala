3a396b28120598c2bbd724d2e82da431cb209330
Matt Hicks <matt@outr.com>
2017-06-16 10:56:12 -0500

    Added temporary debugging code to diagnose a bug in Reactify 2.0

--- example/js/src/main/scala/io/youi/example/ui/HelloWorld.scala
+++ example/js/src/main/scala/io/youi/example/ui/HelloWorld.scala
@@ -13,8 +13,32 @@ object HelloWorld extends UIExampleScreen with UIScreen {
       value := "Hello, World!"
       font.size := 48.0
       fill := Color.DarkBlue
+
+      position.x.attachAndFire(x => scribe.info(s"X: $x"))
+      position.y.attachAndFire(y => scribe.info(s"Y: $y"))
+      size.width.attachAndFire(w => scribe.info(s"W: $w"))
+      size.height.attachAndFire(h => scribe.info(s"H: $h"))
+      size.measured.width.attachAndFire(w => scribe.info(s"MW: $w"))
+      size.measured.height.attachAndFire(h => scribe.info(s"MH: $h"))
+      renderer.position.center.attachAndFire(c => scribe.info(s"C: $c"))
+
       position.center := renderer.position.center
       position.middle := renderer.position.middle
+
+      event.click.on {
+        val x = position.x()
+        val y = position.y()
+        val w = size.width()
+        val h = size.height()
+        val mw = size.measured.width()
+        val mh = size.measured.height()
+        scribe.info(s"X: $x")
+        scribe.info(s"Y: $y")
+        scribe.info(s"W: $w")
+        scribe.info(s"H: $h")
+        scribe.info(s"MW: $mw")
+        scribe.info(s"MH: $mh")
+      }
     }
     container.children += text
   }

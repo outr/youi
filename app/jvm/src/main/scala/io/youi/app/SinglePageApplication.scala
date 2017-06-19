03bc71c681f0812cd09c0d5333474a95a4c6bb44
Matt Hicks <matt@outr.com>
2017-06-19 13:57:29 -0500

    Addition of jsdeps support to SinglePageApplication.

--- app/jvm/src/main/scala/io/youi/app/SinglePageApplication.scala
+++ app/jvm/src/main/scala/io/youi/app/SinglePageApplication.scala
@@ -13,12 +13,13 @@ trait SinglePageApplication extends ServerApplication {
   protected def excludeDotHTML: Boolean = true
   protected def templateDirectory: File
   protected def appJSContent: Content
+  protected def appJSDepsContent: Option[Content] = None
   protected def appJSMapContent: Content = appJSContent match {
     case c: FileContent => c.copy(file = new File(c.file.getParentFile, s"${c.file.getName}.map"))
     case c: URLContent => c.copy(url = new URL(s"${c.url.toString}.map"))
     case c => throw new RuntimeException(s"Unsupported Content $c. You must directly override the appJSMapContent method.")
   }
-  protected def appJSFileName: String = s"${getClass.getSimpleName.replaceAllLiterally("$", "").toLowerCase}.js"
+  protected def appJSFileName: String = s"${getClass.getSimpleName.replaceAllLiterally("$", "").toLowerCase}"
   protected def appJSMethod: String
   protected def scriptPaths: List[String] = Nil
   protected def responseMap(httpConnection: HttpConnection): Map[String, String] = Map.empty
@@ -59,8 +60,11 @@ trait SinglePageApplication extends ServerApplication {
     val neverCacheManager = CachingManager.NotCached
     val lastModifiedManager = CachingManager.LastModified()
 
-    handler.matcher(path.exact(s"/$appJSFileName")).caching(lastModifiedManager).resource(appJSContent)
-    handler.matcher(path.exact(s"/$appJSFileName.map")).caching(lastModifiedManager).resource(appJSMapContent)
+    handler.matcher(path.exact(s"/$appJSFileName.js")).caching(lastModifiedManager).resource(appJSContent)
+    handler.matcher(path.exact(s"/$appJSFileName.js.map")).caching(lastModifiedManager).resource(appJSMapContent)
+    appJSDepsContent.foreach { content =>
+      handler.matcher(path.exact(s"/$appJSFileName-jsdeps.js")).caching(lastModifiedManager).resource(content)
+    }
 
     handler.priority(Priority.Low).handle { httpConnection =>
       val url = httpConnection.request.url
@@ -100,12 +104,18 @@ trait SinglePageApplication extends ServerApplication {
       case (name, value) => s"""<input type="hidden" id="$name" value="$value"/>"""
     }
     val deltas = httpConnection.store.getOrElse[List[Delta]](SinglePageApplication.DeltaKey, Nil)
+    val jsDeps = if (appJSDepsContent.nonEmpty) {
+      s"""<script src="/$appJSFileName-jsdeps.js"></script>"""
+    } else {
+      ""
+    }
     val d = List(
       Delta.InsertLastChild(ByTag("body"),
         s"""
            |${scriptPaths.map(p => s"""<script src="$p"></script>""").mkString("\n")}
            |${responseFields.mkString("\n")}
-           |<script src="/$appJSFileName"></script>
+           |$jsDeps
+           |<script src="/$appJSFileName.js"></script>
            |<script>
            |  $appJSMethod();
            |</script>

b100919a8bdc0b3f64f85d4b5c0595501de42701
Matt Hicks <matt@outr.com>
2017-06-19 12:29:01 -0500

    Added support to Canvas for pre-existing canvas.

--- ui/src/main/scala/io/youi/hypertext/Canvas.scala
+++ ui/src/main/scala/io/youi/hypertext/Canvas.scala
@@ -3,8 +3,18 @@ package io.youi.hypertext
 import org.scalajs.dom._
 import io.youi._
 
-class Canvas extends Component {
-  override protected[youi] val element: html.Canvas = dom.create[html.Canvas]("canvas")
+class Canvas(val element: html.Canvas) extends Component {
+  def this() = this(dom.create[html.Canvas]("canvas"))
 
   init()
+
+  override protected def init(): Unit = {
+    size.actual.width.attach(w => element.width = math.ceil(w).toInt)
+    size.actual.height.attach(h => element.height = math.ceil(h).toInt)
+    super.init()
+  }
+}
+
+object Canvas {
+  def cached(element: html.Canvas): Canvas = Component.cached[html.Canvas, Canvas](element, new Canvas(_))
 }

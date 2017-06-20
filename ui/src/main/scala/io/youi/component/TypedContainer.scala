b0408c444d9eff053ccfcfa30ac37efbf83aba18
Matt Hicks <matt@outr.com>
2017-06-20 16:04:47 -0500

    Added TypedContainer to allow type-safe child hierarchy.

---
+++ ui/src/main/scala/io/youi/component/TypedContainer.scala
@@ -0,0 +1,13 @@
+package io.youi.component
+
+import io.youi.theme.ContainerTheme
+import reactify.Var
+
+class TypedContainer[T <: Component] extends AbstractContainer {
+  override type Child = T
+
+  override lazy val theme: Var[_ <: ContainerTheme] = Var(Container)
+
+  def children: Var[Vector[T]] = childEntries
+}
+

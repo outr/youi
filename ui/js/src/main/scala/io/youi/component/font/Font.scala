0fac599fb319a3aca9307c7b2a77e07f49478b7c
Matt Hicks <matt@outr.com>
2017-07-28 15:53:22 -0500

    Improvements to text selection in Text / Font.

--- ui/js/src/main/scala/io/youi/component/font/Font.scala
+++ ui/js/src/main/scala/io/youi/component/font/Font.scala
@@ -73,10 +73,23 @@ case class TextPaths(paths: Vector[TextPath]) extends Drawable {
     case (tp, index) => TextPath(tp.char, index, tp.path.shift(boundingBox.adjustX, boundingBox.adjustY))
   })
 
+  private def touchingPath(x: Double, y: Double, x1: Double, y1: Double, x2: Double, y2: Double): Option[TouchData] = {
+    val centerX = x1 + ((x2 - x1) / 2.0)
+    val centerY = y1 + ((y2 - y1) / 2.0)
+    if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+      val deltaX = x - centerX
+      val deltaY = y - centerY
+      Some(TouchData(deltaX, deltaY, BoundingBox.distance(x, centerX, y, centerY)))
+    } else {
+      None
+    }
+  }
+
   def touching(x: Double, y: Double): Vector[Touching] = {
     val ry = boundingBox.height - y
     paths.flatMap { tp =>
-      tp.path.boundingBox.touching(x, ry).map(d => Touching(tp, d))
+      val bb = tp.path.boundingBox
+      touchingPath(x, ry, bb.x1, 0.0, bb.x2, boundingBox.height).map(d => Touching(tp, d))
     }.sortBy(_.data.distance)
   }
 

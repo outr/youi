ee533d78fb58ff9b9a47e76f9e2d137f6b10cbf4
Matt Hicks <matt@outr.com>
2017-09-23 18:26:18 -0500

    Minor restructuring of ScrollSupport and added support to disable vertical or horizontal defaulting to horizontal disabled.

--- canvas/js/src/main/scala/io/youi/component/ScrollSupport.scala
+++ canvas/js/src/main/scala/io/youi/component/ScrollSupport.scala
@@ -3,30 +3,37 @@ package io.youi.component
 import reactify._
 
 trait ScrollSupport extends Component {
+  object scroll {
+    object horizontal {
+      val enabled: Var[Boolean] = Var(false)
+      def apply(delta: Double): Unit = {
+        val value = offset.x + delta
+        val min = 0
+        val max = size.measured.width() - size.width()
+        offset.x.static(math.max(max, math.min(min, value)))
+      }
+    }
+    object vertical {
+      val enabled: Var[Boolean] = Var(true)
+      def apply(delta: Double): Unit = {
+        val value = offset.y + delta
+        val min = 0
+        val max = -(size.measured.height() - size.height())
+        offset.y.static(math.max(max, math.min(min, value)))
+      }
+    }
+  }
+
   // Wheel support
   event.pointer.wheel.attach { evt =>
-    scrollVertical(-evt.delta.y)
-    scrollHorizontal(evt.delta.x)
+    if (scroll.vertical.enabled()) scroll.vertical(-evt.delta.y)
+    if (scroll.horizontal.enabled()) scroll.horizontal(evt.delta.x)
 
     evt.stopPropagation()
     evt.preventDefault()
   }
 
   event.gestures.pointers.dragged.attach { pointer =>
-    scrollVertical(pointer.deltaY)
-  }
-
-  def scrollVertical(delta: Double): Unit = {
-    val value = offset.y + delta
-    val min = 0
-    val max = -(size.measured.height() - size.height())
-    offset.y.static(math.max(max, math.min(min, value)))
-  }
-
-  def scrollHorizontal(delta: Double): Unit = {
-    val value = offset.x + delta
-    val min = 0
-    val max = size.measured.width() - size.width()
-    offset.x.static(math.max(max, math.min(min, value)))
+    if (scroll.vertical.enabled()) scroll.vertical(pointer.deltaY)
   }
 }

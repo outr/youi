993b433c275947c8ebf71ab5ede8c7cf66af61ed
Matt Hicks <matt@outr.com>
2017-10-03 23:07:45 -0500

    Added proper detection of devicePixelRatio and backingStoreRatio.

--- ui/js/src/main/scala/io/youi/ui.scala
+++ ui/js/src/main/scala/io/youi/ui.scala
@@ -4,23 +4,33 @@ import io.youi.util.CanvasPool
 import org.scalajs.dom.window
 import reactify._
 
+import scala.concurrent.duration._
 import scala.scalajs.js
 
 object ui {
-  lazy val devicePixelRatio: Double = window.devicePixelRatio
-  lazy val backingStoreRatio: Double = CanvasPool.withCanvas(1.0, 1.0) { canvas =>
+  def devicePixelRatio: Double = window.devicePixelRatio
+  def backingStoreRatio: Double = CanvasPool.withCanvas(1.0, 1.0) { canvas =>
+    def opt(d: js.Dynamic): Option[Double] = d.asInstanceOf[js.UndefOr[Double]].toOption
     val ctx = canvas.context.asInstanceOf[js.Dynamic]
-    val webkit = ctx.webkitBackingStorePixelRatio.asInstanceOf[Option[Double]]
-    val moz = ctx.mozBackingStorePixelRatio.asInstanceOf[Option[Double]]
-    val ms = ctx.msBackingStorePixelRatio.asInstanceOf[Option[Double]]
-    val o = ctx.oBackingStorePixelRatio.asInstanceOf[Option[Double]]
-    val default = ctx.backingStorePixelRatio.asInstanceOf[Option[Double]]
+    val webkit = opt(ctx.webkitBackingStorePixelRatio)
+    val moz = opt(ctx.mozBackingStorePixelRatio)
+    val ms = opt(ctx.msBackingStorePixelRatio)
+    val o = opt(ctx.oBackingStorePixelRatio)
+    val default = opt(ctx.backingStorePixelRatio)
     webkit.orElse(moz).orElse(ms).orElse(o).orElse(default).getOrElse(1.0)
   }
-  lazy val ratio: Var[Double] = Var(devicePixelRatio / backingStoreRatio)
+  lazy val displayRatio: Val[Double] = Var(devicePixelRatio / backingStoreRatio)
+  lazy val ratio: Var[Double] = Var(displayRatio)
   lazy val width: Val[Double] = Val(Display.width * ratio)
   lazy val height: Val[Double] = Val(Display.height * ratio)
 
   lazy val center: Val[Double] = Val(width / 2.0)
   lazy val middle: Val[Double] = Val(height / 2.0)
+
+  AnimationFrame.every(1.second) {
+    val r = devicePixelRatio / backingStoreRatio
+    if (r != displayRatio()) {
+      displayRatio.asInstanceOf[Var[Double]] := r
+    }
+  }
 }

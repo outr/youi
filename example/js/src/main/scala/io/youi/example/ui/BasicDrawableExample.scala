91bdf31c898b125248513ace2f45d76901d47bde
Matt Hicks <matt@outr.com>
2017-10-07 12:13:18 -0500

    Testing of Image drawing functionality.

--- example/js/src/main/scala/io/youi/example/ui/BasicDrawableExample.scala
+++ example/js/src/main/scala/io/youi/example/ui/BasicDrawableExample.scala
@@ -2,8 +2,9 @@ package io.youi.example.ui
 
 import io.youi._
 import io.youi.app.screen.UIScreen
-import io.youi.drawable.{Context, Drawable, Group}
+import io.youi.drawable.{Context, Drawable, Group, Transformation}
 import io.youi.font.{GoogleFont, OpenTypeFont}
+import io.youi.image.Image
 import io.youi.paint.Stroke
 
 object BasicDrawableExample extends UIExampleScreen with UIScreen {
@@ -25,16 +26,25 @@ object BasicDrawableExample extends UIExampleScreen with UIScreen {
   }
 
 //  override protected val drawable: Future[Group] = Future.successful(Group(rectangle, text))
-  override protected val drawable = OpenTypeFont.fromURL(GoogleFont.`Open Sans`.`regular`).map { font =>
-    val text = font("Testing", 96.0)
-    val text2 = new Drawable {
+//  override protected val drawable = OpenTypeFont.fromURL(GoogleFont.`Open Sans`.`regular`).map { font =>
+//    val text = font("Testing", 96.0)
+//    val text2 = new Drawable {
+//      override def draw(context: Context, x: Double, y: Double): Unit = {
+//        text.draw(context, x + 200.0, y + 300.0)
+//        context.fill(Color.Green, apply = true)
+//        context.stroke(Stroke(Color.Black, 2.0), apply = true)
+//      }
+//    }
+//    Group(rectangle, basicText, text2)
+//  }
+
+  override protected val drawable = Image("/images/cuteness.jpg").map { image =>
+    val d = new Drawable {
       override def draw(context: Context, x: Double, y: Double): Unit = {
-        text.draw(context, x + 200.0, y + 300.0)
-        context.fill(Color.Green, apply = true)
-        context.stroke(Stroke(Color.Black, 2.0), apply = true)
+        image.draw(context, x + 200.0, y + 300.0)
       }
     }
-    Group(rectangle, basicText, text2)
+    Group(rectangle, basicText, d)
   }
 
   override def path: String = "/examples/basic-drawable.html"

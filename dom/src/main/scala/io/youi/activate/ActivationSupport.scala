02189913200d773b1a6b85601ea0cee2e95699ba
Matt Hicks <matt@outr.com>
2017-10-10 15:10:51 -0500

    Added onChecked and onUnchecked to Activate instructions.

--- dom/src/main/scala/io/youi/activate/ActivationSupport.scala
+++ dom/src/main/scala/io/youi/activate/ActivationSupport.scala
@@ -44,6 +44,8 @@ object ActivationSupport {
   private val TestRegex = """test\((.+)\)""".r
   private val LinkRegex = """link\("(.+)"\)""".r
   private val OnClickRegex = """(.+).onClick\((.+)\)""".r
+  private val OnCheckedRegex = """(.+).onChecked\((.+)\)""".r
+  private val OnUncheckedRegex = """(.+).onUnchecked\((.+)\)""".r
   private val CommentRegex = """(\s*)//(.*)""".r
 
   private val HasClassConditionRegex = """(.+).hasClass\("(.+)"\)""".r
@@ -97,6 +99,14 @@ object ActivationSupport {
     case OnClickRegex(selector, instruction) => {
       val i = parseInstruction(testing, instruction).getOrElse(throw new RuntimeException(s"Failed to parse instruction: [$instruction]"))
       Some(new OnClick(selector, i))
+    }
+    case OnCheckedRegex(selector, instruction) => {
+      val i = parseInstruction(testing, instruction).getOrElse(throw new RuntimeException(s"Failed to parse instruction: [$instruction]"))
+      Some(new OnChecked(selector, i))
+    }
+    case OnUncheckedRegex(selector, instruction) => {
+      val i = parseInstruction(testing, instruction).getOrElse(throw new RuntimeException(s"Failed to parse instruction: [$instruction]"))
+      Some(new OnUnchecked(selector, i))
     }
     case CommentRegex(_) => None
     case _ => throw new RuntimeException(s"Unknown instruction: [$line]")

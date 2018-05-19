7146e372d6617bcf54d35c7329aaa6359a7fc994
Matt Hicks <matt@outr.com>
2018-05-18 21:39:05 -0500

    Forgot to add the set call to create the stored property

--- ui/js/src/main/scala/io/youi/theme/Theme.scala
+++ ui/js/src/main/scala/io/youi/theme/Theme.scala
@@ -8,10 +8,11 @@ trait Theme {
 
   private val store = new MapStore
 
-  private def get[T](name: String): Option[T] = store.get[T](name).orElse(parentTheme().flatMap(_.get[T](name)))
+  private def get[T](name: String): Option[Var[T]] = store.get[Var[T]](name).orElse(parentTheme().flatMap(_.get[T](name)))
 
   protected def style[T](name: String, default: => T, connect: Option[StyleConnect[T]]): Var[T] = {
-    val v = Var[T](parentTheme().flatMap(_.get[T](name)).getOrElse(default))
+    val v = Var[T](parentTheme().flatMap(_.get[T](name)).map(_.get).getOrElse(default))
+    store(name) = v
     connect.foreach(_.init(this, v, name))
     v
   }

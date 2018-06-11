5251302af89049f8c742a0e951d7bbe1c52eb68a
Matt Hicks <matt@outr.com>
2018-06-11 06:44:20 -0500

    Move Selection into selection package

--- ui/js/src/main/scala/io/youi/component/selection/Selection.scala
+++ ui/js/src/main/scala/io/youi/component/selection/Selection.scala
@@ -1,6 +1,7 @@
-package io.youi.component
+package io.youi.component.selection
 
-import io.youi.event.{HTMLEvents, Mouse}
+import io.youi.component.Container
+import io.youi.event.Mouse
 import io.youi.spatial.BoundingBox
 import io.youi.style.{HTMLBorder, HTMLBorderStyle, Visibility}
 import io.youi.{Color, Key, _}
@@ -208,34 +209,6 @@ abstract class Selection[T](root: html.Element,
           }
         }
       }
-    }
-  }
-}
-
-class SelectionListener[T](selection: Selection[T], base: html.Element, deferToRoot: Boolean, includeChildTargets: Boolean) {
-  var active: Boolean = false
-
-  private val events = new HTMLEvents(selection, base)
-
-  events.pointer.down.attach { evt =>
-    if (includeChildTargets || evt.target == base) {
-      val active = selection.down(evt.htmlMouseEvent.getOrElse(throw new RuntimeException("Not a mouse event")))
-      if (deferToRoot && active) {
-        selection.rootListener.active = active
-      } else {
-        this.active = active
-      }
-    }
-  }
-
-  events.pointer.move.attach { evt =>
-    if (active) selection.move(evt.htmlMouseEvent.getOrElse(throw new RuntimeException("Not a mouse event")))
-  }
-
-  events.pointer.up.attach { evt =>
-    if (active) {
-      selection.up(evt.htmlMouseEvent.getOrElse(throw new RuntimeException("Not a mouse event")))
-      active = false
     }
   }
 }

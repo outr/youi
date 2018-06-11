d20324b743ad2304fbe06bfa55a51c7ba6fb9b15
Matt Hicks <matt@outr.com>
2018-06-11 06:33:10 -0500

    Minor code refactoring

--- ui/js/src/main/scala/io/youi/path/Path.scala
+++ ui/js/src/main/scala/io/youi/path/Path.scala
@@ -178,17 +178,3 @@ object Path extends PathBuilder {
 
   def fix(value: Double): Double = value
 }
-
-@js.native
-@JSGlobal("Path2D")
-class Path2D extends js.Object {
-  def closePath(): Unit = js.native
-  def moveTo(x: Double, y: Double): Unit = js.native
-  def lineTo(x: Double, y: Double): Unit = js.native
-  def bezierCurveTo(cp1x: Double, cp1y: Double, cp2x: Double, cp2y: Double, x: Double, y: Double): Unit = js.native
-  def quadraticCurveTo(cpx: Double, cpy: Double, x: Double, y: Double): Unit = js.native
-  def arc(x: Double, y: Double, radius: Double, startAngle: Double, endAngle: Double, anticlockwise: Boolean = js.native): Unit = js.native
-  def arcTo(x1: Double, y1: Double, x2: Double, y2: Double, radius: Double): Unit = js.native
-  def ellipse(x: Double, y: Double, radiusX: Double, radiusY: Double, rotation: Double, startAngle: Double, endAngle: Double, anticlockwise: Boolean = js.native): Unit = js.native
-  def rect(x: Double, y: Double, width: Double, height: Double): Unit = js.native
-}
--- ui/js/src/main/scala/io/youi/theme/Theme.scala
+++ ui/js/src/main/scala/io/youi/theme/Theme.scala
@@ -48,21 +48,3 @@ trait Theme extends StringifyImplicits {
 object Theme extends Theme {
   override protected def defaultParentTheme: Theme = Theme
 }
-
-class StyleProp[T](val name: String, parent: Var[Theme], default: => T) {
-  val option: Var[Option[T]] = Var(None)
-  val value: Val[T] = Val[T](option().orElse(parent.get.get[T](name).map(_.get)).getOrElse(default))
-
-  def apply(): T = value()
-  def get: T = apply()
-  def :=(value: => T): Unit = option := Option(value)
-  def set(value: => T): Unit = option := Option(value)
-  def clear(): Unit = option := None
-
-  def attach(f: T => Unit,
-             priority: Double = Observer.Priority.Normal): Observer[T] = value.attach(f, priority)
-  def observe(observer: Observer[T]): Observer[T] = value.observe(observer)
-  def detach(observer: Observer[T]): Unit = value.detach(observer)
-  def attachAndFire(f: T => Unit): Observer[T] = value.attachAndFire(f)
-  def changes(observer: ChangeObserver[T]): Observer[T] = value.changes(observer)
-}

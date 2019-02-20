c15a34954f2b10c63564318bebd1fbd0a570ea8f
Matt Hicks <matt@outr.com>
2019-02-20 08:10:51 -0600

    Added ability to disable Blocked

--- core/shared/src/main/scala/io/youi/Blocked.scala
+++ core/shared/src/main/scala/io/youi/Blocked.scala
@@ -4,24 +4,30 @@ import scala.concurrent.{Future, Promise}
 import scribe.Execution.global
 
 object Blocked {
+  var enabled: Boolean = true
+
   private var map = Map.empty[Any, Future[_]]
 
-  def apply[Result](keys: Any*)(f: => Future[Result]): Future[Result] = synchronized {
-    val previous = Future.sequence(keys.map(map.getOrElse(_, Future.successful(()))))
-    val future = if (previous.isCompleted) {
-      f
-    } else {
-      val promise = Promise[Result]
-      previous.onComplete { _ =>
-        val next: Future[Result] = f
-        next.onComplete(promise.complete)
+  def apply[Result](keys: Any*)(f: => Future[Result]): Future[Result] = if (enabled) {
+    synchronized {
+      val previous = Future.sequence(keys.map(map.getOrElse(_, Future.successful(()))))
+      val future = if (previous.isCompleted) {
+        f
+      } else {
+        val promise = Promise[Result]
+        previous.onComplete { _ =>
+          val next: Future[Result] = f
+          next.onComplete(promise.complete)
+        }
+        promise.future
       }
-      promise.future
-    }
 
-    keys.foreach { key =>
-      map += key -> future
+      keys.foreach { key =>
+        map += key -> future
+      }
+      future
     }
-    future
+  } else {
+    f
   }
 }

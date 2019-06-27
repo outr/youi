f22cfa039023f725971bc74591f10615e2933c30
Matt Hicks <matt@outr.com>
2019-06-27 14:41:31 -0500

    Fixed padding and added margin Added placeholder to HTMLTextInput

--- ui/js/src/main/scala/io/youi/theme/ComponentTheme.scala
+++ ui/js/src/main/scala/io/youi/theme/ComponentTheme.scala
@@ -13,10 +13,29 @@ trait ComponentTheme extends Theme {
   val background: StyleProp[Paint] = style[Paint]("background", Paint.none, StyleConnect.style[Paint], updatesRendering = true)
 
   object padding {
-    lazy val left: StyleProp[Double] = style[Double]("padding.left", 0.px, StyleConnect.style[Double])
-    lazy val right: StyleProp[Double] = style[Double]("padding.right", 0.0, StyleConnect.style[Double])
-    lazy val top: StyleProp[Double] = style[Double]("padding.top", 0.0, StyleConnect.style[Double])
-    lazy val bottom: StyleProp[Double] = style[Double]("padding.bottom", 0.0, StyleConnect.style[Double])
+    lazy val left: StyleProp[Double] = style[Double]("padding-left", 0.px, StyleConnect.style[Double])
+    lazy val right: StyleProp[Double] = style[Double]("padding-right", 0.0, StyleConnect.style[Double])
+    lazy val top: StyleProp[Double] = style[Double]("padding-top", 0.0, StyleConnect.style[Double])
+    lazy val bottom: StyleProp[Double] = style[Double]("padding-bottom", 0.0, StyleConnect.style[Double])
+
+    def :=(f: => Double): Unit = set(f)
+
+    def set(f: => Double): Unit = {
+      left.set(f)
+      right.set(f)
+      top.set(f)
+      bottom.set(f)
+    }
+
+    lazy val width: Val[Double] = Val(left() + right())
+    lazy val height: Val[Double] = Val(top() + bottom())
+  }
+
+  object margin {
+    lazy val left: StyleProp[Double] = style[Double]("margin-left", 0.px, StyleConnect.style[Double])
+    lazy val right: StyleProp[Double] = style[Double]("margin-right", 0.0, StyleConnect.style[Double])
+    lazy val top: StyleProp[Double] = style[Double]("margin-top", 0.0, StyleConnect.style[Double])
+    lazy val bottom: StyleProp[Double] = style[Double]("margin-bottom", 0.0, StyleConnect.style[Double])
 
     def :=(f: => Double): Unit = set(f)
 
--- ui/js/src/main/scala/io/youi/theme/HTMLTextInputTheme.scala
+++ ui/js/src/main/scala/io/youi/theme/HTMLTextInputTheme.scala
@@ -4,4 +4,5 @@ import io.youi.theme.mixins.HTMLFontTheme
 
 trait HTMLTextInputTheme extends HTMLComponentTheme with HTMLFontTheme {
   lazy val value: StyleProp[String] = style[String]("value", "", StyleConnect.field[String], updatesTransform = false)
+  lazy val placeholder: StyleProp[String] = style[String]("placeholder", "", StyleConnect.field[String], updatesTransform = false)
 }

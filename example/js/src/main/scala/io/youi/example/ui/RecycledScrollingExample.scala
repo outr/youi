2f302fd6df570db73f2baddb8a36371178588b03
Matt Hicks <matt@outr.com>
2019-07-04 14:52:12 -0500

    Continued work on Recycled Scrolling

--- example/js/src/main/scala/io/youi/example/ui/RecycledScrollingExample.scala
+++ example/js/src/main/scala/io/youi/example/ui/RecycledScrollingExample.scala
@@ -3,9 +3,10 @@ package io.youi.example.ui
 import io.youi.Color
 import io.youi.component.{Component, Container, HTMLTextView}
 import io.youi.example.screen.UIExampleScreen
+import io.youi.layout.VerticalLayout
 import io.youi.net._
-import io.youi.style.{Display, HTMLBorder, HTMLBorderStyle, Visibility}
-import reactify.Var
+import io.youi.style.{Display, HTMLBorder, HTMLBorderStyle, Overflow, Position, Visibility}
+import reactify.{Val, Var}
 
 import scala.concurrent.Future
 import scribe.Execution.global
@@ -15,13 +16,13 @@ class RecycledScrollingExample extends UIExampleScreen {
   override def path: Path = path"/examples/recycled-scrolling.html"
 
   override def createUI(): Future[Unit] = {
-    val scroller = new RecycledScroller[Int, NumberComponent](NumberComponentRenderer)
+    val scroller = new RecycledScroller[Int, NumberComponent](8, NumberComponentRenderer)
     scroller.position.center := container.size.center
     scroller.position.middle := container.size.middle
     scroller.size.width := 1000.0
-    scroller.size.height := 900.0
+    scroller.size.height := 500.0
     scroller.background := Color.LightGray
-    scroller.batchedData := BatchedData((0 until 5000).toList)
+    scroller.batch.data := BatchedData((0 until 5000).toList)
     container.children += scroller
 
     Future.successful(())
@@ -30,7 +31,8 @@ class RecycledScrollingExample extends UIExampleScreen {
   class NumberComponent extends Container {
     val value: Var[Int] = Var(0)
 
-    size.width := parent().map(_.size.width()).getOrElse(0.0)
+    position.`type` := Position.Absolute
+    size.width := 1000.0
     size.height := 50.0
     htmlBorder.radius := 5.0
     htmlBorder := HTMLBorder(1.0, HTMLBorderStyle.Dashed, Color.DarkRed)
@@ -47,31 +49,98 @@ class RecycledScrollingExample extends UIExampleScreen {
     override def createComponent: NumberComponent = new NumberComponent
 
     override def setData(data: Int, component: NumberComponent): Unit = component.value := data
+
+    override def loading(component: NumberComponent): Unit = component.value := -1
   }
 }
 
-class RecycledScroller[T, C <: Component](renderer: RecycledRenderer[T, C]) extends Container {
-  val batchedData: Var[BatchedData[T]] = Var(BatchedData.empty[T])
+class RecycledScroller[T, C <: Component](perPage: Int, renderer: RecycledRenderer[T, C]) extends Container {
+  object batch {
+    val data: Var[BatchedData[T]] = Var(BatchedData.empty[T])
+    val position: Var[Int] = Var(0)
+    val current: Val[Int] = Val(data().total -  position())
+  }
 
-  private var pool = List.empty[C]
-  private def acquire: C = pool.headOption match {
-    case Some(c) => {
-      pool = pool.tail
-      c
+  overflow := Overflow.Hidden
+
+  private val pane1 = new RecycledPane {
+    background := Color.Blue
+  }
+  private val pane2 = new RecycledPane {
+    background := Color.Green
+  }
+  private val pane3 = new RecycledPane {
+    background := Color.Red
+  }
+
+  private var middle = pane1
+  private var top = pane2
+  private var bottom = pane3
+
+  // TODO: create "scroll" in gestures
+  event.pointer.wheel.attach { evt =>
+    middle.position.y.static(middle.position.y() - evt.delta.y)
+    if (evt.delta.y < 0) {      // Scrolling up
+      if (top.position.middle() >= 0.0) {                 // Make top the new middle
+        val pt = top
+        val pm = middle
+        val pb = bottom
+        top = pb
+        middle = pt
+        bottom = pm
+
+        middle.position.y.static(middle.position.y())
+        top.position.bottom := middle.position.top
+        bottom.position.top := middle.position.bottom
+      }
+    } else {                    // Scrolling down
+      if (bottom.position.middle() <= size.height()) {    // Make bottom the new middle
+        val pt = top
+        val pm = middle
+        val pb = bottom
+        top = pm
+        middle = pb
+        bottom = pt
+
+        middle.position.y.static(middle.position.y())
+        top.position.bottom := middle.position.top
+        bottom.position.top := middle.position.bottom
+      }
     }
-    case None => renderer.createComponent
-  }
-  private def release(c: C): Unit = {
-    pool = c :: pool
   }
 
-  batchedData.attachAndFire { data =>
+  batch.data.attachAndFire { data =>
     if (data.nonEmpty) {
-      data.get(0, 1).map { list =>
-        val test = acquire
-        test.position.bottom := size.height
-        renderer.setData(list.head, test)
-        children += test
+      data.get(0, perPage).foreach(middle.update)
+    }
+  }
+
+  pane1.position.bottom := size.height
+  pane2.position.bottom := pane1.position.top
+  pane3.position.top := pane1.position.bottom
+
+  children ++= List(pane1, pane2, pane3)
+
+  class RecycledPane extends Container {
+    var start: Int = 0
+    var end: Int = 0
+    val components: Vector[C] = (0 until perPage).toVector.map(_ => renderer.createComponent)
+    components.foreach(renderer.loading)
+
+    layout := new VerticalLayout
+    size.width := RecycledScroller.this.size.width
+    size.height := components.last.position.bottom
+    children ++= components
+
+    def update(data: Vector[T]): Unit = {
+      components.zipWithIndex.foreach {
+        case (c, index) => data.lift(perPage - (index + 1)) match {
+          case Some(t) => {
+            renderer.setData(t, c)
+            renderer.show(c)
+          }
+          case None => renderer.hide(c)
+        }
       }
     }
   }
@@ -80,6 +149,7 @@ class RecycledScroller[T, C <: Component](renderer: RecycledRenderer[T, C]) exte
 trait RecycledRenderer[T, C <: Component] {
   def createComponent: C
   def setData(data: T, component: C): Unit
+  def loading(component: C): Unit
   def show(component: C): Unit = component.display := Display.Block
   def hide(component: C): Unit = component.display := Display.None
 }
@@ -88,19 +158,19 @@ trait BatchedData[T] {
   def total: Int
   def isEmpty: Boolean = total == 0
   def nonEmpty: Boolean = total > 0
-  def get(start: Int, end: Int): Future[List[T]]
+  def get(start: Int, end: Int): Future[Vector[T]]
 }
 
 object BatchedData {
   def empty[T]: BatchedData[T] = new BatchedData[T] {
     override def total: Int = 0
 
-    override def get(start: Int, end: Int): Future[List[T]] = throw new RuntimeException("Cannot get data from empty batch")
+    override def get(start: Int, end: Int): Future[Vector[T]] = throw new RuntimeException("Cannot get data from empty batch")
   }
 
   def apply[T](data: Seq[T]): BatchedData[T] = new BatchedData[T] {
     override def total: Int = data.length
 
-    override def get(start: Int, end: Int): Future[List[T]] = Future.successful(data.slice(start, end).toList)
+    override def get(start: Int, end: Int): Future[Vector[T]] = Future.successful(data.slice(start, end).toVector)
   }
 }

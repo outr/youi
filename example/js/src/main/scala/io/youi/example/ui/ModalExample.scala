e4d0d81d721243c49f235fb5f22987121b49b69a
Matt Hicks <matt@outr.com>
2019-07-09 12:50:57 -0500

    Replaced Bootstrap Modal with custom Modal for UIModal for better flexibility

--- example/js/src/main/scala/io/youi/example/ui/ModalExample.scala
+++ example/js/src/main/scala/io/youi/example/ui/ModalExample.scala
@@ -2,12 +2,15 @@ package io.youi.example.ui
 
 import io.youi._
 import io.youi.component.{Container, HTMLTextView, UIModal}
+import io.youi.easing.Easing
 import io.youi.example.screen.UIExampleScreen
 import io.youi.font.GoogleFont
 import io.youi.net._
-import io.youi.style.{HTMLBorder, HTMLBorderStyle}
+import io.youi.style.{Display, HTMLBorder, HTMLBorderStyle}
+import io.youi.task._
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scribe.Execution.global
 
 class ModalExample extends UIExampleScreen {
@@ -15,7 +18,7 @@ class ModalExample extends UIExampleScreen {
   override def path: Path = path"/examples/modal.html"
 
   override def createUI(): Future[Unit] = GoogleFont.`Open Sans`.load().map { fnt =>
-    val myModal = UIModal(new Container {
+    val myModal = new Container with UIModal {
       size.width := 800.0
       size.height := 500.0
       background := Color.White
@@ -30,7 +33,30 @@ class ModalExample extends UIExampleScreen {
         position.center := 400.0
         position.middle := 250.0
       }
-    })
+
+      position.top := 100.0
+
+      override def show(): Unit = {
+        position.bottom := 0.0
+        opacity := 0.0
+        parallel(
+          position.top to 100.0 in 250.millis easing Easing.quadraticOut,
+          opacity to 1.0 in 250.millis
+        ).start(this)
+
+        super.show()
+      }
+
+      override def hide(): Unit = {
+        UIModal.active := None
+        parallel(
+          position.bottom to 0.0 in 500.millis,
+          opacity to 0.0 in 250.millis
+        )
+          .andThen(synchronous(display := Display.None))
+          .start(this)
+      }
+    }
     val button = new HTMLTextView {
       value := "Open Modal"
       font := fnt
--- ui/js/src/main/scala/io/youi/component/UIModal.scala
+++ ui/js/src/main/scala/io/youi/component/UIModal.scala
@@ -1,61 +1,71 @@
 package io.youi.component
 
-import io.youi.component.bootstrap.Modal
+import io.youi.{Color, ui}
 import io.youi.component.extras.HTMLComponent
-import io.youi.util.Time
+import io.youi.style.{Display, Position}
+import org.scalajs.dom.html
 import reactify.Var
 
-import scala.concurrent.duration._
-import scribe.Execution.global
+trait UIModal {
+  this: HTMLComponent[_ <: html.Element] =>
 
-class UIModal[C <: Component] private(val component: C,
-                                      backdrop: String,
-                                      keyboard: Boolean,
-                                      focus: Boolean,
-                                      show: Boolean ) extends Container with Modal {
-  val verticallyCentered: Var[Boolean] = Var(false)
+  val closeOnBackdrop: Var[Boolean] = Var(true)
 
-  element.classList.add("modal")
-  element.classList.add("fade")     // TODO: make configurable
-  element.setAttribute("role", "dialog")
-  element.setAttribute("aria-hidden", "true")
+  position.center := ui.position.center
+  position.middle := ui.position.middle
+  position.`type` := Position.Fixed
+  display := Display.None
 
-  private val document = new Container {
-    element.classList.add("modal-dialog")
-    element.classList.add("modal-lg")
-    element.setAttribute("role", "document")
-    size.width := component.size.width
-    size.height := component.size.height
-  }
-  children += document
-
-  private val htmlElement = HTMLComponent.element(component)
-  htmlElement.classList.add("modal-content")
-  document.children += component
-
-  verticallyCentered.attach { b =>
-    if (b) {
-      htmlElement.classList.add("modal-dialog-centered")
-    } else {
-      htmlElement.classList.remove("modal-dialog-centered")
+  def show(): Unit = {
+    UIModal.active := Some(this)
+    if (position.z() <= UIModal.backdrop.position.z()) {
+      position.z.static(UIModal.backdrop.position.z() + 1)
     }
+    display := Display.Block
   }
 
-  override protected def init(): Unit = {
-    super.init()
-
-    Time.delay(10.millis).foreach { _ =>
-      initialize(backdrop, keyboard, focus, show)
-    }
+  def hide(): Unit = {
+    UIModal.active := None
+    display := Display.None
   }
 }
 
 object UIModal {
-  def apply[C <: Component](component: C,
-                            backdrop: String = "true",
-                            keyboard: Boolean = true,
-                            focus: Boolean = true,
-                            show: Boolean = false): UIModal[C] = {
-    new UIModal[C](component, backdrop, keyboard, focus, show)
+  val active: Var[Option[UIModal]] = Var(None)
+
+  val backdrop: Container = new Container {
+    background := Color.Black.withAlpha(0.5)
+    position.x := 0.0
+    position.y := 0.0
+    position.z := 100
+    position.`type` := Position.Fixed
+    size.width := ui.size.width
+    size.height := ui.size.height
+    display := Display.None
+
+    ui.children += this
+
+    event.click.on {
+      active().foreach { m =>
+        if (m.closeOnBackdrop()) {
+          hide()
+        }
+      }
+    }
   }
+
+  private var previous = Option.empty[UIModal]
+  active.attach { a =>
+    previous.foreach(p => p.hide())
+    a match {
+      case Some(modal) => {
+        modal.show()
+        backdrop.display := Display.Block
+      }
+      case None => backdrop.display := Display.None
+    }
+    previous = a
+  }
+
+  def hide(): Unit = active := None
 }

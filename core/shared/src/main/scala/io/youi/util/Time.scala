ae1336ab677149f5567589ccb2b5096b4050475d
Matt Hicks <matt@outr.com>
2019-09-06 15:40:34 -0500

    Added Time.waitFor

--- core/shared/src/main/scala/io/youi/util/Time.scala
+++ core/shared/src/main/scala/io/youi/util/Time.scala
@@ -1,7 +1,9 @@
 package io.youi.util
 
-import scala.concurrent.Future
-import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeoutException
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 
 /**
   * Cross-platform functionality for dealing with time
@@ -14,4 +16,31 @@ object Time {
     * @return Future[Unit] that will be complete after `duration`
     */
   def delay(duration: FiniteDuration): Future[Unit] = io.youi.YouIPlatform.delay(duration.toMillis)
+
+  /**
+    * Waits for a condition to be true before returning.
+    *
+    * @param condition the condition function that is being waited for true. Should return false until ready.
+    * @param frequency the frequency to check the condition (defaults to 100 milliseconds)
+    * @param timeout the timeout to delay before a TimeoutException is thrown (defaults to 5 minutes)
+    * @param started the timestamp of when this was started (defaults to current time)
+    * @param ec the execution context to use
+    * @return Future[Unit]
+    */
+  def waitFor(condition: => Boolean,
+              frequency: FiniteDuration = 100.milliseconds,
+              timeout: FiniteDuration = 5.minutes,
+              started: Long = System.currentTimeMillis())
+             (implicit ec: ExecutionContext): Future[Unit] = {
+    val elapsed = System.currentTimeMillis() - started
+    if (condition) {
+      Future.successful(())
+    } else if (elapsed > timeout.toMillis) {
+      throw new TimeoutException("Timed out waiting for condition!")
+    } else {
+      delay(frequency).flatMap { _ =>
+        waitFor(condition, frequency, timeout, started)
+      }
+    }
+  }
 }

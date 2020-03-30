528b84e59aca4952a4c55763be333a33da875314
Matt Hicks <matt@courio.com>
2020-03-29 19:07:33 -0500

    Added type to InternalContainerSupport

--- gui/src/main/scala/io/youi/component/support/InternalContainerSupport.scala
+++ gui/src/main/scala/io/youi/component/support/InternalContainerSupport.scala
@@ -3,8 +3,8 @@ package io.youi.component.support
 import io.youi.component.Component
 import io.youi.component.feature.ContainerFeature
 
-trait InternalContainerSupport {
+trait InternalContainerSupport[Child <: Component] {
   this: Component =>
 
-  protected lazy val children: ContainerFeature[Component] = new ContainerFeature[Component](this)
+  protected lazy val children: ContainerFeature[Child] = new ContainerFeature[Child](this)
 }

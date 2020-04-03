66a46851bd64ee5a37b6bf5e222c7c3db0c5b6ac
Matt Hicks <matt@courio.com>
2020-04-02 21:41:46 -0500

    Updated MaxSizeFeature to trigger measurement

--- gui/src/main/scala/io/youi/component/feature/MaxSizeFeature.scala
+++ gui/src/main/scala/io/youi/component/feature/MaxSizeFeature.scala
@@ -4,6 +4,6 @@ import io.youi.component.Component
 import io.youi.component.types.SizeProperty
 
 class MaxSizeFeature(override val component: Component) extends Feature {
-  lazy val width: SizeProperty = new SizeProperty(component.element.style.maxWidth, component.element.style.maxWidth_=)
-  lazy val height: SizeProperty = new SizeProperty(component.element.style.maxHeight, component.element.style.maxHeight_=)
+  lazy val width: SizeProperty = new SizeProperty(component.element.style.maxWidth, component.element.style.maxWidth_=, component.measure.trigger)
+  lazy val height: SizeProperty = new SizeProperty(component.element.style.maxHeight, component.element.style.maxHeight_=, component.measure.trigger)
 }

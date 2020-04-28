51e5553f271e971a605a6b1f6384ac9f5c572596
Matt Hicks <matt@courio.com>
2020-04-28 09:21:48 -0500

    Another update to ui size support for better flexibility

--- gui/src/main/scala/io/youi/ui.scala
+++ gui/src/main/scala/io/youi/ui.scala
@@ -4,20 +4,23 @@ import io.youi.component.Container
 import io.youi.component.support.SizeSupport
 import io.youi.component.types.Prop
 import io.youi.event.{EventSupport, Swipe}
+import io.youi.spatial.Size
 import io.youi.task.TaskSupport
 import org.scalajs.dom._
+import reactify.Var
 
 object ui extends Container(document.body) with SizeSupport with EventSupport with TaskSupport {
   override lazy val title: Prop[String] = new Prop(document.title, document.title_=)
   lazy val swipe: Swipe = new Swipe(ui, ui.event, onlyTouch = true)
+  val windowMeasurer: Var[Size => Size] = Var(_.set(window.innerWidth, window.innerHeight))
+
+  private val mutableSize = Size.mutable()
 
   measure.on {
-    size.width @= windowWidth
-    size.height @= windowHeight
+    val measured = windowMeasurer()(mutableSize)
+    size.width @= measured.width
+    size.height @= measured.height
   }
-
-  protected def windowWidth: Double = window.innerWidth
-  protected def windowHeight: Double = window.innerHeight
 
   measure.trigger()
   window.addEventListener("resize", (_: Event) => {

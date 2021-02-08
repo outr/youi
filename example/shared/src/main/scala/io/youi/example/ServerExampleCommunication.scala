e9ffe868c736cf370455b4a630806168a8cb893a
Matt Hicks <matt@courio.com>
2021-02-08 13:34:47 -0800

    Minor code cleanup

--- example/shared/src/main/scala/io/youi/example/ServerExampleCommunication.scala
+++ example/shared/src/main/scala/io/youi/example/ServerExampleCommunication.scala
@@ -4,8 +4,8 @@ import scala.concurrent.Future
 
 trait ServerExampleCommunication {
   def reverse(value: String): Future[String]
-  def time: Future[Long]
-  def counter: Future[Int]
+  def time(): Future[Long]
+  def counter(): Future[Int]
   def broadcast(message: String): Future[Unit]
   def logIn(username: String, password: String): Future[Option[String]]
 }
